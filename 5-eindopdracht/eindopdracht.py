e16ce08912996c7b635537661d6b2d8c479e0864
Wouter Baas <Wouter.Baas@student.rocva.nl>
2021-12-09 11:51:35 +0100

    python eindopdracht :)

--- 5-eindopdracht/eindopdracht.py
+++ 5-eindopdracht/eindopdracht.py
@@ -1,24 +1,29 @@
 import time
 #de variabelen voor de klok hier kan je ook de starttijd instellen
-seconden=50
-minuten=37
-uur=2
+uur= int(input('enter uur'))
+minuten= int(input('enter minuten'))
+seconden= int(input('enter seconden'))
+
+def showklok():
+    print('%02d'%i, '%02d'%j, '%02d'%k, sep=":")
 
 # de klok loop
-while seconden <60:
-    seconden +=1
-    # de '%02d'% zorgt ervoor dat de getallen altijd uit 2 bestaan 01, 02 bijv.
-    # de sep zorgt dat tussen de uur minuten en seconden een : komt.
-    # en de time.sleep zorgt ervoor dat er iedere seconden een seconden bij komt.
-    print('%02d'%uur, '%02d'%minuten, '%02d'%seconden, sep=":")
-    time.sleep(1)
-    #deze if zorgen ervoor dat als de seconden op 60 komt er 1 minuut bijkomt en de seconden weer gerzet word.
-    #dit geld ook voor de andere if's
-    if seconden == 59:
-        minuten +=1
-        seconden =-1
-        if minuten == 60:
-            uur +=1
-            minuten =0
-            if uur ==24:
-                uur = 0
+while True:
+    for i in range(uur,24):
+            
+        for j in range(minuten, 60):
+           
+            for k in range(seconden, 60):  
+                time.sleep(1)
+                showklok()
+            seconden=0 
+        minuten = 0
+    uur = 0
+                
+# de if's zorgen dat de int na het instellen van de tijdnaar 0 gezet word
+# de '%02d'% zorgt ervoor dat de getallen die in de console getoont woorden altijd uit 2 bestaan 01, 02 bijv.
+# de sep zorgt dat tussen de uur minuten en seconden een : komt.
+# en de time.sleep zorgt ervoor dat de loop iedere seconden een pauze neemt van een seconden.
+
+
+
